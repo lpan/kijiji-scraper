468d5e3d89d420a77b426d0bed7d3e8ea627d5a1
Jack Luo <Admin@iMac.local>
2016-07-29 02:37:19 -0400

    1st version of Kijiji Scraper

---
+++ master/crawly.py
@@ -0,0 +1,21 @@
+##################### HEAD #######################
+
+import requests
+from lxml import html
+
+################### FUNCTIONS ####################
+
+
+# This function fetches the website and returns an html object
+def get_url(url):
+
+    try:
+        if "http://" not in url: url = "http://"+ url
+        response = requests.get(url)
+    except:
+        print "[ERROR] Connection error."
+        print "[ERROR] Program quitting."
+        quit()
+
+    response = html.fromstring(response.text)
+    return response
--- master/run.py
+++ master/run.py
@@ -1,53 +1,102 @@
+##################### HEAD #######################
+
 import requests
 from lxml import html
+
 import re
+import csv
+
+from crawly import *
+
+#################### CLASSES #####################
+
 
 class MainListing(object):
-    def __init__(self, title, date, price, link):
+    def __init__(self, title, price, date, link):
         self.title = title
-        self.date = date
         self.price = price
+        self.date = date
         self.link = link
 
 
 class SubListing(MainListing):
-    def __init__(self, for_sale, description):
+    def __init__(self, date, for_sale, description):
+        self.date = date
         self.for_sale = for_sale
         self.description = description
 
+################### FUNCTIONS ####################
 
 
-# This function fetches the website and returns an html object
-def get_url(url):
+# This function parses the search query into a valid Kijiji link
+def get_kijiji_query():
 
-    try:
-        if "http://" not in url: url = "http://"+ url
-        response = requests.get(url)
-    except:
-        print "[ERROR] Connection error."
-        print "[ERROR] Program quitting."
-        quit()
+    REGION = "b-grand-montreal/" #montreal
+    CODE = "k0l80002/" #montreal
 
-    response = html.fromstring(response.text)
+    while True:
+        # link = raw_input("Enter search query >>> ")
+        link = "macbook retina 13 256"
+        link = link.strip().replace(" ", "-") + "/"
+        try: 
+            response = get_url("http://www.kijiji.ca/"+ REGION + link + CODE)
+            break
+        except:
+            print "[Error] Search error."
+            print "[Info] Try again."
+            continue
+
     return response
 
-response = get_url("www.kijiji.ca/b-grand-montreal/macbook-13-retina/k0l80002")
 
-titles = response.xpath('//*[@id="MainContainer"]/div[3]/div[2]/div//div[2]/div/div[2]/a/text()')
+# This function obtains the properties of each listing and returns a list of MainListing instances
+def get_main_listings(response):
 
-print ""
+    titles = response.xpath('//div[not(@class="search-item top-feature ")]/div/div/div[@class="title"]/a/text()')
+    titles = [_.strip() for _ in titles]
 
-for title in titles:
-    title = title.strip()
-    print title
+    dates = response.xpath('//div[not(@class="search-item top-feature ")]/div/div/div/span[@class="date-posted"]/text()')
+    dates = [_.strip() for _ in dates]
 
-print ""
+    prices = response.xpath('//div[not(@class="search-item top-feature ")]/div/div/div[@class="price"]/text()')
+    prices = [_.strip().replace(u"\xa0", u"").replace(u"$",u"").replace(u",",".") for _ in prices]
 
-prices = response.xpath('//*[@id="MainContainer"]/div[3]/div[2]/div//div[2]/div/div[1]/text()')
+    links = response.xpath('//div[not(@class="search-item top-feature ")]/div/div/div[@class="title"]/a/@href')
+    links = [_.strip() for _ in links]
 
-for price in prices:
-    if re.compile(u'\s+[,.0-9]+\s+[,.0-9]+') not in price: print "ERROR"
-    price = price.strip()
-    print price
+    main_listings = [MainListing(titles[i], prices[i], dates[i], links[i]) for i, _ in enumerate(titles)]
 
-print ""
+    return main_listings
+
+
+# This function prints listings
+def print_listings(listings):
+
+    print "-" * 80
+
+    for i, main_listing in enumerate(main_listings):
+        i += 1
+        print "[{:2}]".format(i), main_listing.title
+    print "-" * 80
+
+    for i, main_listing in enumerate(main_listings):
+        i += 1
+        print "[{:2}]".format(i), main_listing.price
+    print "-" * 80
+
+    for i, main_listing in enumerate(main_listings):
+        i += 1
+        print "[{:2}]".format(i), main_listing.date
+    print "-" * 80         
+
+    for i, main_listing in enumerate(main_listings):
+        i += 1
+        print "[{:2}]".format(i), main_listing.link
+    print "-" * 80
+
+##################### MAIN #######################
+
+response = get_kijiji_query()
+main_listings = get_main_listings(response)
+print_listings(main_listings)
+
